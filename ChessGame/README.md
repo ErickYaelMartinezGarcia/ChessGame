7f7456e2e5df88e512f0384f311ad2f65f980e54
Erick Yael Martinez Garcia <168610567+ErickYaelMartinezGarcia@users.noreply.github.com>
2024-12-21 19:23:50 +0000

    creacion de mov de peones

--- ChessGame/README.md
+++ ChessGame/README.md
@@ -3,3 +3,18 @@
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+
+
+----------------
+datos de proyecto tareas asignadas
+- cuando se toque una pieza se deben de visualizar sus moviemientos que puede realizar 
+ -- al realizar estos movimientos si se puede comer alguna pieza debe de pintarse en rojo
+
+- generar algoritmos de movimiento de cada una de las piezas
+
+- las blancas mueven primero
+
+- primer movimiento de peones es de dos celdas hacia enfrente 
+ 
+ - puedo guardar copias de mis arrays en un objetos para deshacer moviemientos 
+ 
--- ChessGame/src/App.vue
+++ ChessGame/src/App.vue
@@ -10,6 +10,7 @@ export default {
         ['','','','','','','',''],
         ['','','','','','','',''],
         ['','','','','','','',''],
+        ['','','','','','','',''],
         ['bP','bP','bP','bP','bP','bP','bP','bP'],
         ['bT','bH','bB','bK','bQ','bB','bH','bT'],
       ],
@@ -18,19 +19,62 @@ export default {
   methods: {
    getOddCells(i,rI){
       if(rI == 1 ||rI == 3||rI == 5||rI == 7){
-        return (i+1)%2 === 0
+        return (i)%2 === 0
       }
-   return i%2 === 0
+   return (i+1)%2 === 0
    },
    printFigure(str){
-        let color = str.startWith('w')
+    
+        let color = str.startsWith('w')
        if(color){
         switch(str){
             case 'wT': 
-              return 'fa-solid fa-chess-rook' 
+              return 'fa-regular fa-chess-rook bg-white rounded p-1';
+            case 'wH': 
+              return 'fa-regular fa-chess-knight bg-white rounded p-1';
+            case 'wB': 
+              return 'fa-regular fa-chess-bishop bg-white rounded p-1';
+              case 'wK':
+              return 'fa-regular fa-chess-king bg-white rounded p-1';
+            case 'wQ': 
+              return 'fa-regular fa-chess-queen bg-white rounded p-1';
+            case 'wP': 
+              return 'fa-regular fa-chess-pawn bg-white rounded p-1';
             }
-       } 
+       }
+
+       switch(str){
+            case 'bT': 
+              return 'fa-solid fa-chess-rook';
+            case 'bH': 
+              return 'fa-solid fa-chess-knight';
+            case 'bB': 
+              return 'fa-solid fa-chess-bishop';
+            case 'bK':
+              return 'fa-solid fa-chess-king';
+            case 'bQ': 
+              return 'fa-solid fa-chess-queen';
+            case 'bP': 
+              return 'fa-solid fa-chess-pawn';
+            }
+
        
+    },
+
+    moveFigure(pza,...coordinates){
+      if(pza.endsWith('P')){
+        debugger
+        console.log(this.cells[coordinates[0]])
+        console.log(coordinates[0])
+        this.cells[coordinates[0]][coordinates[1]] = ''
+        this.cells[coordinates[0]+1][coordinates[1]] = pza
+        console.log(this.cells)
+      }
+    },
+    movePawn(coordinates){
+        //get and return new coordinates [column,row] 
+        //evaluate if have some to eat
+        return [coordinates[0+1],coordinates[1]]
     }
   },
   mounted() {
@@ -40,15 +84,14 @@ export default {
 </script>
 
 <template>
-  <div class=" col-9 bg-dark border-dark border rounded-3 mx-auto" style="height:88vh" >
+  <div class=" col-8  border-dark border rounded-3 mx-auto" style="height:88vh" >
   <table class="col-12 h-100 rounded-3">
-  <template v-for="(row,rI) in cells" :key="rI">
-  <tr class="border" style="height:12.5%">
-      <template v-for="(column,cI) in row" :key="cI">
-            <td class="border" :style="{ 'background-color': getOddCells(cI, rI) ? 'white' : 'grey' }">
-              <div class="my-auto w-100 text-justify text-center">
-              
-                <span class="fs-1 fa-solid fa-chess-pawn"></span>
+  <template v-for="(column,cI) in cells" :key="cI">
+  <tr class=" row p-0 m-0" style="height:12.5%">
+      <template v-for="(row,rI) in column" :key="rI">
+            <td class=" col" :style="{ 'background-color': getOddCells(rI, cI) ? 'white' : 'grey' }">
+              <div class="d-flex justify-content-center align-items-center h-100">
+                <span class="fs-1" :class="printFigure(row)" @click="moveFigure(row,cI,rI)"></span>
               </div>
             </td>
       </template>  
